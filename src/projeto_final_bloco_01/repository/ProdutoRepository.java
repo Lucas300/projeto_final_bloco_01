017e4781514526c3b514f49b42e827b91f1d3967
Lucas300 <ludaniel.sd@gmail.com>
2024-12-19 11:09:52 -0300

    Segunda Etapa do Projeto final - inserindo a interface

---
+++ src/projeto_final_bloco_01/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package projeto_final_bloco_01.repository;
+
+import projeto_final_bloco_01.Model.Produto;
+
+public interface ProdutoRepository {
+	public void listarTodas();
+	public void cadastrar(Produto produto);
+	public void atualizar(Produto produto);
+	public void deletar(int numero);
+}
